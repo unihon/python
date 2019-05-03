d3993658b563ab7c5abfc5fcd5c644273f8c884d
unihon <unihon@outlook.com>
2019-05-03 16:06:55 +0800

    ts update

--- ts/ts.py
+++ ts/ts.py
@@ -1,11 +1,23 @@
-from pynput import keyboard
+from pynput import keyboard, mouse
+import win32con
+import win32gui
 import pyperclip
 import reqm
 
-KEYWORK_0 = "Key.ctrl_l"
-KEYWORK_1 = "c"
-KEYWORK_2 = "q"
+# hwnd ------------------------------------------------
+
+ts_hwnd = win32gui.GetForegroundWindow()
+
+# value ------------------------------------------------
+
+KEYWORK_KEY = "Key.ctrl_l"
+KEYWORK_SWITCH = "Key.space"
+KEYWORK_TRANSLA = "c"
+KEYWORK_QUIT = "q"
+STATUS = True
 keyStack = []
+
+# key ------------------------------------------------
 
 def on_press(key):
     try:
@@ -29,8 +41,11 @@ def on_release(key):
 # 对按键栈进行是否符合组合键的判断
 # 第一个按下和第一个放开的组合
 def keyCheck(key_s):
+    global STATUS
     if keyStack:
-        if keyStack[0] == KEYWORK_0 and key_s == KEYWORK_1:
+        # 组合键匹配
+        if STATUS and keyStack[0] == KEYWORK_KEY and key_s == KEYWORK_TRANSLA:
+            win32gui.ShowWindow(ts_hwnd, win32con.SW_RESTORE)
             query_s = getText()
             if query_s:
                 print("Loading...")
@@ -42,8 +57,29 @@ def keyCheck(key_s):
             # 匹配一次之后清空按键栈，解决线程堵塞，只监听到释放一个按键的问题
             keyStack.clear()
             return False
-        elif keyStack[0] == KEYWORK_0 and key_s == KEYWORK_2:
+
+        elif keyStack[0] == KEYWORK_KEY and key_s == KEYWORK_SWITCH:
+            if STATUS:
+                STATUS = False
+                print("""
+┌┄┄┄┄┄┄┄┬┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┐
+┆ PAUSE ┆ Ctrl+Space switch ┆
+└┄┄┄┄┄┄┄┴┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┘
+""")
+                return False
+            else:
+                win32gui.ShowWindow(ts_hwnd, win32con.SW_RESTORE)
+                STATUS = True
+                print("""
+┌┄┄┄┄┄┄┄┬┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┐
+┆ GO    ┆ Ctrl+Space switch ┆
+└┄┄┄┄┄┄┄┴┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┘
+""")
+                return False
+
+        elif keyStack[0] == KEYWORK_KEY and key_s == KEYWORK_QUIT:
             return True
+
 
 # 按键进栈
 def keyStackIn(key_s):
@@ -61,6 +97,14 @@ def keyStackOut(key_s):
         else:
             keyStack.pop()
 
+# mouse ------------------------------------------------
+
+def on_click(x, y, button, pressed):
+    now_hwnd = win32gui.GetForegroundWindow()
+    if not pressed and now_hwnd != ts_hwnd:
+        win32gui.ShowWindow(ts_hwnd, win32con.SW_MINIMIZE)
+
+# text ------------------------------------------------
 
 def getText():
     raw = pyperclip.paste()
@@ -70,12 +114,22 @@ def getText():
         return None
 
 print("""
-┌┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┐
-┆ Globle Ctrl+C translate. ┆
-┆ Ctrl+Q exit.             ┆
-└┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┘
+┌┄┄┄┄┄┄┄┄┄┄┄┄┄┄┬┄┄┄┄┄┄┄┄┄┐
+┆ Ctrl+C       ┆ transla ┆
+├┄┄┄┄┄┄┄┄┄┄┄┄┄┄┼┄┄┄┄┄┄┄┄┄┤
+┆ Ctrl+Space   ┆ switch  ┆
+├┄┄┄┄┄┄┄┄┄┄┄┄┄┄┼┄┄┄┄┄┄┄┄┄┤
+┆ Ctrl+Q       ┆ quit    ┆
+└┄┄┄┄┄┄┄┄┄┄┄┄┄┄┴┄┄┄┄┄┄┄┄┄┘
 """)
 
+# listen ------------------------------------------------
+
+# non-blocking
+listener = mouse.Listener(on_click = on_click)
+listener.start()
+
+# Collect events until released
 with keyboard.Listener(on_press = on_press, on_release = on_release) as listener:
     try:
         listener.join()
